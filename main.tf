398895db60fe1bb37c522c97c24c1963b9869ec8
51r <petar.spasov@hashicorp.com>
2022-06-13 13:34:11 +0300

    initial commit

---
+++ main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "eu-west-2"
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0538d5d3468787d06"
+
+}
+
+resource "aws_security_group" "sg_80" {
+  name = "peter_80"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                         = var.ami_id
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.sg_80.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name  = "Peter-Test"
+    email = "spasov@hashicorp.com"
+  }
+}
+
+output "website_ip" {
+  value = aws_instance.web.public_ip
+}
