fba59240a4f06e58c260070b34ecbc01af0be887
51r <petar.spasov@hashicorp.com>
2022-06-16 09:19:17 +0300

    added name prefix tag

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,22 +1,34 @@
+
+# Specify AMI ID output from Packer
 variable "ami_id" {
   type    = string
   default = "ami-0e8040f700cdfdd53"
 }
 
+# Specify CIDR Block for Subnet
 variable "subnet_cidr" {
   type    = string
   default = "10.0.0.0/24"
 
 }
 
+# Specify CIDR block for VPC
 variable "vpc_cidr" {
   type    = string
   default = "10.0.0.0/16"
 
 }
 
+# Specify the email for resource's tags
 variable "email" {
   type    = string
   default = "spasov@hashicorp.com"
 
+}
+
+# Specify the prefix name for the AWS resources
+variable "name" {
+  type = string
+  default = "Peter"
+  
 }
