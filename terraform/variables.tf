d161d997697207aa6cdcf4c2921c36d986c71309
51r <petar.spasov@hashicorp.com>
2022-06-16 09:05:32 +0300

    added vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,21 @@ variable "ami_id" {
   type    = string
   default = "ami-0e8040f700cdfdd53"
 }
+
+variable "subnet_cidr" {
+  type    = string
+  default = "10.0.0.0/24"
+
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.0.0.0/16"
+
+}
+
+variable "email" {
+  type    = string
+  default = "spasov@hashicorp.com"
+
+}
